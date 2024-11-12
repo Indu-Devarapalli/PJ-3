1164c6d4858a590e8eb2724aa379c4d881059af5
InduDevarapally <Idd854@ftr.com>
2024-11-12 21:45:05 +0530

    first commit

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    assest:['./assests/fonts']
+}
+module.exports = {
+    project: {
+      android: {
+        sourceDir: './android',  // Adjust this path if your android folder is in a different location
+      },
+    },
+  };
